82c109b151d57ad17d88b5f6a8b14b00eb39863c
Danya Generalov <danya02@me.com>
2017-01-25 14:04:02 +0300

    Rewriting quiz for wxPython.

--- quiz.py
+++ quiz.py
@@ -1,85 +1,81 @@
 #!/usr/bin/python3
-from tkinter import *
-from tkinter import messagebox
+import wx
 import random
 import json
-conf = json.load(open("config.json"))
-quiz = conf["quizzes"][0]
-root = Tk()
-text = Text(root, wrap=WORD, height=1)
-text.grid(row=0, column=0, columnspan=2)
-button1 = Button(root, text="START", width=32)
-button1.grid(row=1, column=0)
-button2 = Button(root, text="START", width=32)
-button2.grid(row=1, column=1)
-button3 = Button(root, text="START", width=32)
-button3.grid(row=2, column=0)
-button4 = Button(root, text="START", width=32)
-button4.grid(row=2, column=1)
 
 
-def update_text(field, newtext):
-    if isinstance(field, Text):
-        field.config(state=NORMAL)
-        field.delete("0.0", END)
-        field.insert(END, newtext)
-        field.config(state=DISABLED)
-    else:
-        field.config(text=newtext)
+class MyFrame(wx.Frame):
+    def __init__(self, *args, **kwds):
+        wx.Frame.__init__(self, *args, **kwds)
+        self.label_1 = wx.StaticText(self, wx.ID_ANY, (
+            "Press any of the buttons to begin."))
+        self.button_1 = wx.Button(self, wx.ID_ANY, ("START"))
+        self.button_2 = wx.Button(self, wx.ID_ANY, ("START"))
+        self.button_3 = wx.Button(self, wx.ID_ANY, ("START"))
+        self.button_4 = wx.Button(self, wx.ID_ANY, ("START"))
 
-update_text(text, "Press any of the buttons to begin.")
+        self.__set_properties()
+        self.__do_layout()
+        self.conf = json.load(open("config.json"))
+        self.quiz = self.conf["quizzes"][0]
 
+    def __set_properties(self):
+        self.SetTitle(("Quiz"))
+        self.SetFocus()
+        self.button_1.Bind(wx.EVT_BUTTON, self.ask_question)
+        self.button_2.Bind(wx.EVT_BUTTON, self.ask_question)
+        self.button_3.Bind(wx.EVT_BUTTON, self.ask_question)
+        self.button_4.Bind(wx.EVT_BUTTON, self.ask_question)
 
-def right_answer():
-    global button1
-    global button2
-    global button3
-    global button4
-    global text
-    messagebox.showinfo("Correct answer!",
-                        "You answered the question correctly!")
-    for i in [button1, button2, button3, button4]:
-        update_text(i, "RESTART")
-        i.config(command=ask_question)
-    update_text(text, "Press any of the buttons to ask another question.")
+    def __do_layout(self):
+        sizer_1 = wx.BoxSizer(wx.VERTICAL)
+        grid_sizer_1 = wx.GridSizer(2, 2, 0, 0)
+        sizer_1.Add(self.label_1, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.EXPAND, 0)
+        grid_sizer_1.Add(self.button_1, 0, wx.ALIGN_CENTER, 0)
+        grid_sizer_1.Add(self.button_2, 0, wx.ALIGN_CENTER, 0)
+        grid_sizer_1.Add(self.button_3, 0, wx.ALIGN_CENTER, 0)
+        grid_sizer_1.Add(self.button_4, 0, wx.ALIGN_CENTER, 0)
+        sizer_1.Add(grid_sizer_1, 1, 0, 0)
+        self.SetSizer(sizer_1)
+        sizer_1.Fit(self)
+        self.Layout()
 
+    def right_answer(self):
+        c = wx.MessageBox("You answered the question correctly!", "Correct answer!")
+        c.ShowModal()
+        c.destroy()
+        for i in [self.button_1, self.button_2, self.button_3, self.button_4]:
+            i.SetValue("RESTART")
+            i.Bind(wx.EVT_BUTTON, self.ask_question)
+        self.label_1.SetValue("Press any of the buttons to ask another question.")
 
-def wrong_answer():
-    messagebox.showerror("Wrong answer!",
-                         "You gave an incorrect answer!")
+    def wrong_answer(self):
+        c = wx.MessageBox("You gave an incorrect answer!", "Wrong answer!", wx.ICON_ERROR)
+        c.ShowModal()
+        c.destroy()
 
+    def ask_question(self):
+        buttons = [self.button_1, self.button_2, self.button_3, self.button_4]
+        str_question = random.choice(list(self.quiz["questions"].keys()))
+        str_answer = self.quiz["questions"][str_question]
+        str_question = self.quiz["question_form"].format(str_question)
+        print(str_question, str_answer)
+        false_answers = []
+        for i in range(3):
+            a = random.choice(list(self.quiz["questions"].values()))
+            if not a == str_answer and a not in false_answers:
+                false_answers += [a]
+        random.shuffle(buttons)
+        true_button = buttons.pop(random.randint(0, len(buttons)-1))
+        false_buttons = buttons
+        self.label_1.SetValue(str_question)
+        true_button.SetValue(str_answer)
+        true_button.Bind(wx.EVT_BUTTON, self.right_answer)
+        for i, j in zip(false_answers, false_buttons):
+            j.SetValue(i)
+            j.Bind(wx.EVT_BUTTON, self.wrong_answer)
 
-def ask_question():
-    global quiz
-    global text
-    global button1
-    global button2
-    global button3
-    global button4
-    buttons = [button1, button2, button3, button4]
-    str_question = random.choice(list(quiz["questions"].keys()))
-    str_answer = quiz["questions"][str_question]
-    str_question = quiz["question_form"].format(str_question)
-    print(str_question, str_answer)
-    false_answers = []
-    for i in range(3):
-        a = random.choice(list(quiz["questions"].values()))
-        if not a == str_answer and a not in false_answers:
-            false_answers += [a]
-    random.shuffle(buttons)
-    true_button = buttons.pop(random.randint(0, len(buttons)-1))
-    false_buttons = buttons
-    update_text(text, str_question)
-    update_text(true_button, str_answer)
-    true_button.config(command=right_answer)
-    for i, j in zip(false_answers, false_buttons):
-        update_text(j, i)
-        j.config(command=wrong_answer)
-    true_button.flash()
-
-button1.config(command=ask_question)
-button2.config(command=ask_question)
-button3.config(command=ask_question)
-button4.config(command=ask_question)
-
-root.mainloop()
+app = wx.App(redirect=True)
+top = MyFrame()
+top.Show()
+app.MainLoop()

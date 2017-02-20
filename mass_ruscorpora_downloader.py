ac3cc2f266a3008a370005f939f90e909cfea91d
Danya Generalov <danya02@me.com>
2017-02-20 22:03:38 +0300

    Fixing errors.

--- mass_ruscorpora_downloader.py
+++ mass_ruscorpora_downloader.py
@@ -4,15 +4,19 @@ import requests
 import os
 import time
 import sys
+
 time_start = time.time()
 
 def log(obj, newline=True):
     global args
-    if args.v:
-        if newline:
-            print(obj, flush=True)
-        else:
-            print(obj, end="", flush=True)
+    try:
+        if args.v:
+            if newline:
+                print(obj, flush=True)
+            else:
+                print(obj, end="", flush=True)
+    except:
+        pass
 
 parser = argparse.ArgumentParser(
     description="download a list of objects from ruscorpora's parallel corpus")
@@ -26,15 +30,16 @@ parser.add_argument(
     '-v', "--verbose", action='store_true',
     help='print status information')
 
+global args
 args = parser.parse_args()
 log("Arguments parsed.")
 log("Loading input file... ", newline=False)
-list = [i.split(" ") for i in args.output.read().split("\n")]
+list = [i.split(" ") for i in args.list.read().split("\n")]
 log("done.")
 
 log("Making directory `" + args.output + "`...", newline=False)
 try:
-    os.makedir(args.output)
+    os.mkdir(args.output)
     log("done.")
 except FileExistsError:
     log("FAILED - USING EXISTING.")
@@ -44,19 +49,19 @@ log("Starting list evaluation... ", newline=False)
 for i in list:
     log("done.")
     log("Opening file `" + i[0] + ".html` for writing... ")
-    with open(i[0]+".html", "w") as file:
+    with open(i[0]+".html", "wb") as file:
         log("Composing URL... ", newline=False)
         url = "http://search2.ruscorpora.ru/search.xml?dpp=&spp=&spd=&text=lexgramm&mode=para&sort=gr_tagging&env=alpha&"
         for j,k in zip(i[1:], range(1, len(i))):
             url += "&parent{n}=0&level{n}=0&lex{n}={text}&gramm{n}=&flags{n}=&sem{n}=".format(text=j, n=k)
         log("Composed URL is: `"+url+"`.")
         log("Sending request... ", newline=False)
-        with requests.get(url) as request:
-            log("done.")
-            log("Writing file... ", newline=False)
-            file.write(request.content)
-            log("done.")
-            log("Closing file... ", newline=False)
+        request = requests.get(url)
+        log("done.")
+        log("Writing file... ", newline=False)
+        file.write(request.content)
+        log("done.")
+        log("Closing file... ", newline=False)
 log("done.")
 time_spent = time.time() - time_start
 log("---DONE---")
